11272beaee39374c9dcd8ad17eb504690ebbfef3
AmirBana <amir220.h@gmail.com>
2022-04-28 01:26:35 +0430

    surface generating fixed (Almost :)!

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -7,7 +7,7 @@ public class Generator : MonoBehaviour
 	[SerializeField] GameObject normalSurface;
 	[SerializeField] Transform surfaceHolder;
 	[SerializeField] float yBound = 2.4f;
-	[SerializeField] float minXBound = -3.2f, maxXBound = 3.2f;
+	[SerializeField] float minXBound = -3f, maxXBound = 3f;
 	private int pastScore;
 	// Start is called before the first frame update
 	void Start()
@@ -25,13 +25,14 @@ public class Generator : MonoBehaviour
 	}
 	public void GenerateSurface()
 	{
-		int amount = Random.Range(1, 4);
+		int amount = Random.Range(1, 5);
 		for (int i = 0; i < amount; i++)
 		{
 			float xPos = Random.Range(minXBound, maxXBound);
 			//float yPos = Random.Range(transform.position.y, transform.position.y + yBound);
 			Vector2 spawnPos = new Vector2(xPos, transform.position.y);
 			Instantiate(normalSurface, spawnPos, normalSurface.transform.rotation, surfaceHolder);
+			transform.position = new Vector3(transform.position.x, transform.position.y + 0.8f, transform.position.z);
 		}
 		GameManager.Instance.generateSurface = false;
 	}
